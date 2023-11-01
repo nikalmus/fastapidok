fecc8538ed0266068fc55900c9456799a2e2e707
Nik Musaelian <nmusaelian@awninc.com>
2023-10-31 18:36:04 -0600

    new schema with timestamps, users table

--- backend/app/db/migrations/versions/ee95fc57690b_create_main_tables.py
+++ backend/app/db/migrations/versions/ee95fc57690b_create_main_tables.py
@@ -5,15 +5,48 @@ Revises:
 Create Date: 2023-10-29 02:46:51.234162
  
 """
+from typing import Tuple
 from alembic import op
 import sqlalchemy as sa
-
  
-# revision identifiers, used by Alembic
-revision = 'ee95fc57690b'
+revision = "ee95fc57690b"
 down_revision = None
 branch_labels = None
 depends_on = None
+ 
+ 
+def create_updated_at_trigger() -> None:
+    op.execute(
+        """
+        CREATE OR REPLACE FUNCTION update_updated_at_column()
+            RETURNS TRIGGER AS
+        $$
+        BEGIN
+            NEW.updated_at = now();
+            RETURN NEW;
+        END;
+        $$ language 'plpgsql';
+        """
+    )
+ 
+ 
+def timestamps(indexed: bool = False) -> Tuple[sa.Column, sa.Column]:
+    return (
+        sa.Column(
+            "created_at",
+            sa.TIMESTAMP(timezone=True),
+            server_default=sa.func.now(),
+            nullable=False,
+            index=indexed,
+        ),
+        sa.Column(
+            "updated_at",
+            sa.TIMESTAMP(timezone=True),
+            server_default=sa.func.now(),
+            nullable=False,
+            index=indexed,
+        ),
+    )
  
  
 def create_cleanings_table() -> None:
@@ -24,12 +57,62 @@ def create_cleanings_table() -> None:
         sa.Column("description", sa.Text, nullable=True),
         sa.Column("cleaning_type", sa.Text, nullable=False, server_default="spot_clean"),
         sa.Column("price", sa.Numeric(10, 2), nullable=False),
+        sa.Column("cleaner_id", sa.Integer, sa.ForeignKey("cleaner.id"), nullable=True),
+        *timestamps(),
+    )
+    op.execute(
+        """
+        CREATE TRIGGER update_cleanings_modtime
+            BEFORE UPDATE
+            ON cleanings
+            FOR EACH ROW
+        EXECUTE PROCEDURE update_updated_at_column();
+        """
+    )
+ 
+ 
+def create_cleaner_table() -> None:
+    op.create_table(
+        "cleaner",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column("name", sa.Text, nullable=False, index=True),
+        *timestamps(),
+    )
+ 
+ 
+def create_users_table() -> None:
+    op.create_table(
+        "users",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column("username", sa.Text, unique=True, nullable=False, index=True),
+        sa.Column("email", sa.Text, unique=True, nullable=False, index=True),
+        sa.Column("email_verified", sa.Boolean, nullable=False, server_default="False"),
+        sa.Column("salt", sa.Text, nullable=False),
+        sa.Column("password", sa.Text, nullable=False),
+        sa.Column("is_active", sa.Boolean(), nullable=False, server_default="True"),
+        sa.Column("is_superuser", sa.Boolean(), nullable=False, server_default="False"),
+        *timestamps(),
+    )
+    op.execute(
+        """
+        CREATE TRIGGER update_user_modtime
+            BEFORE UPDATE
+            ON users
+            FOR EACH ROW
+        EXECUTE PROCEDURE update_updated_at_column();
+        """
     )
  
  
 def upgrade() -> None:
+    create_updated_at_trigger()
+    create_cleaner_table()
     create_cleanings_table()
+    create_users_table()
  
  
 def downgrade() -> None:
+    op.drop_table("users")
     op.drop_table("cleanings")
+    op.drop_table("cleaner")
+    op.execute("DROP FUNCTION update_updated_at_column")
